89d658ce1b128e0febe309e2f0fcc2cfd93c7e08
Priyanka Gugale <priyanka@datatorrent.com>
2016-06-29 14:41:24 +0530

    Making hdfs folder and other changes

--- package/scripts/dtrts_client.py
+++ package/scripts/dtrts_client.py
@@ -23,6 +23,7 @@ import sys
 import os
 import logging
 from resource_management import *
+from resource_management.libraries.resources.hdfs_resource import HdfsResource
 
 class DtRtsClient(Script):
   def install(self, env):
@@ -30,6 +31,7 @@ class DtRtsClient(Script):
     env.set_params(params)
     self.install_datatorrent_repo()
     self.install_packages(env)
+    self.add_hdfs_folder(env)
 
   def configure(self, env, upgrade_type=None):
     import params
@@ -50,15 +52,29 @@ class DtRtsClient(Script):
 
   def install_datatorrent_repo(self):
       import os
+      import platform
       distribution = platform.linux_distribution()[0].lower()
-      if distribution in ['centos', 'redhat'] and not os.path.exists('/etc/yum.repos.d/datatorrent.repo'):
+      if distribution.lower() in ['centos', 'redhat'] and not os.path.exists('/etc/yum.repos.d/datatorrent.repo'):
           Execute('curl -o /etc/yum.repos.d/datatorrent.repo https://www.datatorrent.com/downloads/repos/yum/repo/datatorrent-rts.repo')
           Execute('curl -sO https://www.datatorrent.com/downloads/repos/apt/keyFile')
           Execute('rpm --import keyFile')
-      elif distribution in ['Ubuntu'] and not os.path.exists('/etc/apt/sources.list.d/datatorrent.list'):
+      elif distribution.lower() in ['ubuntu'] and not os.path.exists('/etc/apt/sources.list.d/datatorrent.list'):
           Execute('echo "deb https://www.datatorrent.com/downloads/repos/apt/ /" > /etc/apt/sources.list.d/datatorrent.list')
           Execute('wget -O - https://www.datatorrent.com/downloads/repos/apt/keyFile | apt-key add -')
           Execute('apt-get update')
+
+  def add_hdfs_folder(self, env):
+      import params
+      import os
+      env.set_params(params)
+      homeDir = '/user/' + params.datatorrent_username
+      params.HdfsResource(homeDir,
+                        type="directory",
+                        action="create_on_execute",
+                        owner=params.datatorrent_username,
+                        group=params.datatorrent_groupname,
+                        mode=0755
+                        )
 
 if __name__ == "__main__":
   DtRtsClient().execute()
--- package/scripts/params.py
+++ package/scripts/params.py
@@ -1,8 +1,45 @@
 from resource_management.libraries.script.script import Script
+from resource_management.libraries.resources.hdfs_resource import HdfsResource
+from resource_management.libraries.functions import conf_select
+from resource_management.libraries.functions import hdp_select
+from resource_management.libraries.functions import get_kinit_path
+from resource_management.libraries.functions.default import default
 
 # server configurations
 config = Script.get_config()
 
+# hadoop default parameters
+hadoop_conf_dir = conf_select.get_hadoop_conf_dir()
+hadoop_bin_dir = hdp_select.get_hadoop_dir("bin")
+hadoop_home = '/usr'
+
 datatorrent_install_dir = config['configurations']['datatorrent-rts-config']['datatorrent.install_dir']
 datatorrent_username = config['configurations']['datatorrent-rts-config']['datatorrent.username']
 datatorrent_groupname = config['configurations']['datatorrent-rts-config']['datatorrent.groupname']
+hdfs_user = config['configurations']['hadoop-env']['hdfs_user']
+hdfs_principal_name = config['configurations']['hadoop-env']['hdfs_principal_name']
+hdfs_user_keytab = config['configurations']['hadoop-env']['hdfs_user_keytab']
+security_enabled = config['configurations']['cluster-env']['security_enabled']
+kinit_path_local = get_kinit_path(default('/configurations/kerberos-env/executable_search_paths', None))
+
+hdfs_site = config['configurations']['hdfs-site']
+default_fs = config['configurations']['core-site']['fs.defaultFS']
+
+dfs_type = default("/commandParams/dfs_type", "")
+
+import functools
+#create partial functions with common arguments for every HdfsResource call
+#to create hdfs directory we need to call params.HdfsResource in code
+HdfsResource = functools.partial(
+  HdfsResource,
+  user=hdfs_user,
+  security_enabled = security_enabled,
+  keytab = hdfs_user_keytab,
+  kinit_path_local = kinit_path_local,
+  hadoop_bin_dir = hadoop_bin_dir,
+  hadoop_conf_dir = hadoop_conf_dir,
+  principal_name = hdfs_principal_name,
+  hdfs_site = hdfs_site,
+  default_fs = default_fs,
+  dfs_type = dfs_type
+ )
